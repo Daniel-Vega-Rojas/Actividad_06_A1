9232db69a870171136deee856bf7a919d95d8fb4
Tulio Cuadrado <tcuadrado@uniguajira.edu.co>
2021-11-22 22:10:28 -0500

    Segundo Commit Tulio Cuadrado 22/11/2021

--- apphoteles/src/app/services/persona.service.ts
+++ apphoteles/src/app/services/persona.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient} from '@angular/common/http'
+
+import { Observable } from 'rxjs';
+
+import {PersonaI} from '../models/PersonaI'
+
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
 
-  constructor() { }
+  api_url = 'http://localhost:4000';
+  base_path = `${this.api_url}/personas`;
+
+
+
+  constructor(
+
+    private http: HttpClient
+    
+  ) { }
+
+  getPersona(): Observable<PersonaI[]> {
+
+    return this.http.get<PersonaI[]>(this.base_path)
+     
+  }
 }
+
+
