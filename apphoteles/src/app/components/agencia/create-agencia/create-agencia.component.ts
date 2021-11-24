6ce4233ad50f69f6a6eb64b6d3b357a42691de8b
Tulio Cuadrado <tcuadrado@uniguajira.edu.co>
2021-11-23 20:15:39 -0500

    commit tulio cuadrado

--- apphoteles/src/app/components/agencia/create-agencia/create-agencia.component.ts
+++ apphoteles/src/app/components/agencia/create-agencia/create-agencia.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AgenciaService } from 'src/app/services/agencia.service';
+import { AgenciaI } from 'src/app/models/AgenciaI';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-create-agencia',
@@ -7,9 +13,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAgenciaComponent implements OnInit {
 
-  constructor() { }
+  public formulario: FormGroup = this.formBuilder.group({
+
+    nombre:     ['', Validators.required],
+    direccion:  ['', Validators.required],
+    telefono:   ['', Validators.required],
+    ciudad:     ['', Validators.required],
+    status:     ['', Validators.required]
+  })
+
+  constructor(
+
+    private formBuilder: FormBuilder,
+    private agenciaService: AgenciaService,
+    private snackBar: MatSnackBar,
+    private roter: Router
+  ) { }
 
   ngOnInit(): void {
+
+
   }
+
+  onSubmit(): void {
+
+    const formValue: AgenciaI = this.formulario.value;
+    this.agenciaService.createAgencia(formValue).subscribe(
+      () => {
+        this.snackBar.open(
+          'Agencia Creada Con Exito ;)', 'OK', {
+            duration: 5000,
+          }
+        );
+        this.roter.navigateByUrl('/agencias');
+      },
+      err => {
+        this.snackBar.open(
+          'La Agencia no fue creado correctamente *f* ','ERROR', {
+            duration: 5000,
+          }
+        )
+      }
+    )
+  } 
 
 }
--- apphoteles/src/app/components/persona/create-persona/create-persona.component.ts
+++ apphoteles/src/app/components/persona/create-persona/create-persona.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonaService } from 'src/app/services/persona.service';
+import { PersonaI } from 'src/app/models/PersonaI';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-create-persona',
@@ -7,9 +13,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePersonaComponent implements OnInit {
 
-  constructor() { }
+  public formulario: FormGroup = this.formBuilder.group({
+
+    nombre:    ['', Validators.required],
+    apellido:   ['', Validators.required],
+    direccion: ['', Validators.required],
+    telefono: ['', Validators.required],
+    status:   ['', Validators.required]
+  })
+
+  constructor(
+
+    private formBuilder: FormBuilder,
+    private personaService: PersonaService,
+    private snackBar: MatSnackBar,
+    private roter: Router
+  ) { }
 
   ngOnInit(): void {
+
+
   }
+
+  onSubmit(): void {
+
+    const formValue: PersonaI = this.formulario.value;
+    this.personaService.createPersona(formValue).subscribe(
+      () => {
+        this.snackBar.open(
+          'Persona Creada Con Exito ;)', 'OK', {
+            duration: 5000,
+          }
+        );
+        this.roter.navigateByUrl('/personas');
+      },
+      err => {
+        this.snackBar.open(
+          'Persona  no fue creado correctamente *f* ','ERROR', {
+            duration: 5000,
+          }
+        )
+      }
+    )
+  } 
 
 }
